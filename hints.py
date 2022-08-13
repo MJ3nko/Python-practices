45a808772a384d0b2c918927e49cdb76d199966a
Neotar <neotar16@gmail.com>
2022-08-13 22:01:08 +0200

    Add readability of large numbers example

--- hints.py
+++ hints.py
@@ -1,7 +1,16 @@
 def type_hint(name: str) -> str:
-    # Make code more readable
+    # Make code more readable.
     # Python 3.5+
-    print(f'Hello {name}')
+    print(f"Hello {name}")
 
 
-type_hint('Anonymous')
+def readability():
+    # Improve readability of large numbers.
+    # Python 3.6+
+    x = 1_000_000
+    y = 1000000
+    print(x, y, x == y)
+
+
+type_hint("Anonymous")
+readability()
