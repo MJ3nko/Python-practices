aae38360a51f9104d3fbb44e21e8828d04c15f96
Neotar <neotar16@gmail.com>
2022-08-13 20:59:07 +0200

    Rename objects to classes_and_objects

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -16,6 +16,10 @@ class Rectangle1:
     def area(self) -> float:
         return self.width * self.height
 
+#  Using data classes, Python will automatically
+#  generate special methods like "_init__" and "__repr__",
+#  reducing a lot of the clutter from your code.
+
 
 @dataclass
 class Rectangle2:
@@ -25,6 +29,9 @@ class Rectangle2:
 
     def area(self) -> float:
         return self.width * self.height
+
+# Instead of cluttering your code with constants, you can create an enumeration using the Enum class.
+# An Enumeration is a set of symbolic names bound to unique, constant values.
 
 
 class Color(Enum):
