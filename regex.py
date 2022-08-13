e45bbb143dae9645d71be4a676464639dbe19a3a
Neotar <neotar16@gmail.com>
2022-08-13 22:00:32 +0200

    Add regex skeleton

---
+++ regex.py
@@ -0,0 +1,39 @@
+import re
+
+
+def re_match(domain):
+    # Return match object, apply at the start of the string
+    match = re.match(r"http[s]?", domain)
+    if match:
+        print(match.group(0))
+
+
+def re_findall(domain):
+    capture_domain = r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]"
+    results = re.findall(capture_domain, domain)
+    if results:
+        for result in results:
+            print(result)
+
+
+def re_search(domain, email, ipv4):
+    # Return match object
+    results = re.search(r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]", domain)
+    email_results = re.search(r"[\w\.-]+@[\w\.-]+", email)
+    ip_results = re.search(r"^((\d){1,3}\.){3}\d{1,3}", ipv4)
+
+    if results:
+        print(results.group(0))
+    if email_results:
+        print(email_results.group(0))
+    if ip_results:
+        print(ip_results.group(0))
+
+
+if __name__ == "__main__":
+    domain = "https://www.google.com"
+    ip = "127.0.0.1"
+    email = input("Input email:")
+    re_match(domain)
+    re_findall(domain)
+    re_search(domain, email, ip)
