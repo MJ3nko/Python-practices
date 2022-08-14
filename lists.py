2648b6c743db7a172dbafde10bb989e53301d8cc
Neotar <neotar16@gmail.com>
2022-08-14 16:48:21 +0200

    Add lists usage examples

--- lists.py
+++ lists.py
@@ -41,6 +41,37 @@ def zip_usage():
     names_and_dogs_names = zip(names, dogs_names)
 
 
+def list_comprehension_usage():
+
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    newlist = [[a, b, c] for a in range(x+1) for b in range(y+1)
+               for c in range(z+1) if (a + b + c) != n]
+    print(newlist)
+
+
+def print_second_largest_number():
+    n = int(input())
+    arr = map(int, input().split())
+    arr_list = list(arr)
+    first = arr_list[0]
+    second = arr_list[0]
+
+    for number in arr_list:
+        if number > first:
+            second = first
+            first = number
+        elif first > number > second:
+            second = number
+
+    print(second)
+
+
 if __name__ == "__main__":
     lists_usage()
     unpack_argument_list()
+    list_comprehension_usage()
+    print_second_largest_number()
