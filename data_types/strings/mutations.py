c49066524d535f5206faf015c453c56e12644a87
Neotar <neotar16@gmail.com>
2022-08-20 00:28:30 +0200

    Add slicing example

---
+++ data_types/strings/mutations.py
@@ -0,0 +1,30 @@
+def guideline():
+    guideline = """
+    Lists are mutable and tuples are immutable.
+    """
+
+    print(guideline)
+
+
+def change_character(string: str, index: int, char: str) -> str:
+    string = list(string)
+    string[index] = char
+    string = "".join(string)
+    return string
+
+
+def slice_string_and_join_character(string: str, index: int, char: str) -> str:
+    string = string[:index] + char + string[index+1:]
+    return string
+
+
+def main():
+    string = input()
+    index, char = input().split()
+    new_string = change_character(string, int(index), char)
+    new_string = slice_string_and_join_character(string, int(index), char)
+    print(new_string)
+
+
+if __name__ == '__main__':
+    main()
