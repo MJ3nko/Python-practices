acbca13f9f992312ed672d003dc0fe72c9beb4bb
Neotar <neotar16@gmail.com>
2022-08-22 10:49:13 +0200

    Add rangoli design and number base conversion example

---
+++ data_types/strings/number_base.py
@@ -0,0 +1,23 @@
+def add_spaces(value, width):
+    value = " " * (width - len(value)) + value
+    return value
+
+
+def print_formatted(number):
+    width = len(str(bin(number).split(sep="b")[1]))
+    for value in range(1, number + 1):
+        decimal = str(value)
+        octal = str(oct(value).split(sep="o")[1])
+        hexadecimal = str(hex(value).split(sep="x")[1]).upper()
+        binary = str(bin(value).split(sep="b")[1])
+
+        decimal = add_spaces(decimal, width)
+        octal = add_spaces(octal, width)
+        hexadecimal = add_spaces(hexadecimal, width)
+        binary = add_spaces(binary, width)
+        print(f"{decimal} {octal} {hexadecimal} {binary}")
+
+
+if __name__ == '__main__':
+    number = int(input())
+    print_formatted(number)
---
+++ data_types/strings/rangoli_design.py
@@ -0,0 +1,37 @@
+def print_top_half(size, highest_char):
+    for distance in range(size - 1, -1, -1):
+        write_line(distance, highest_char)
+
+
+def print_bottom_half(size, highest_char):
+    for distance in range(1, size):
+        write_line(distance, highest_char)
+
+
+def write_line(distance, highest_char):
+    minus_chars = "-" * (2 * (distance))
+    middle_char = ord("a") + distance
+    alpha_chars = return_alpha_chars(highest_char, middle_char)
+    left_side = f"{minus_chars}{alpha_chars}"
+    right_side = left_side[::-1]
+    print(f"{left_side}{chr(middle_char)}{right_side}")
+
+
+def return_alpha_chars(starting_char, end_char):
+    if end_char == starting_char:
+        return ""
+    line = ""
+    for char in range(starting_char, end_char, -1):
+        line += f"{chr(char)}-"
+    return line
+
+
+def print_rangoli(size):
+    highest_char = ord("a") + size - 1
+    print_top_half(size, highest_char)
+    print_bottom_half(size, highest_char)
+
+
+if __name__ == '__main__':
+    number = int(input())
+    print_rangoli(number)
