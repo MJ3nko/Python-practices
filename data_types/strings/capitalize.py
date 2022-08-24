254a5e01e2e38949d59b7e716e1fdca55a4e66f2
Neotar <neotar16@gmail.com>
2022-08-24 19:47:06 +0200

    Add more strings challenges solutions

---
+++ data_types/strings/capitalize.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+def first_solution(line: str):
+    words = line.split(sep=" ")
+    sentence = []
+    for word in words:
+        sentence.append(word.capitalize())
+    sentence = " ".join(sentence)
+    return sentence
+
+
+def second_solution(line: str):
+    words = line.split(sep=" ")
+    sentence = []
+    for word in words:
+        word = word[:1].upper() + word[1:]
+        sentence.append(word)
+    sentence = " ".join(sentence)
+    return words
+
+
+if __name__ == '__main__':
+    line = input()
+    result = first_solution(line)
+    print(result)
---
+++ data_types/strings/polynomial.py
@@ -0,0 +1,58 @@
+def execute_operation(result, operation, number):
+    if operation == "+" or operation == "":
+        result += number
+    elif operation == "-":
+        result -= number
+    elif operation == "*":
+        result *= number
+    elif operation == "/":
+        result /= number
+    return result
+
+
+def poly_sum(x, p):
+    result = 0
+    operation = ""
+    for word in p:
+        if "*x**" in word:
+            times = int(word.split("*x**")[0])
+            potenca = int(word.split("*x**")[1])
+            number = times * (x ** int(potenca))
+            result = execute_operation(result, operation, number)
+        elif "**" in word:
+            potenca = int(word.split("**")[1])
+            number = x ** potenca
+            result = execute_operation(result, operation, number)
+        elif word in ["+", "-", "*", "/"]:
+            operation = word
+        elif len(word) == 1:
+            if "x" in word:
+                number = x
+                result = execute_operation(result, operation, number)
+            else:
+                number = int(word)
+                result = execute_operation(result, operation, number)
+    return result
+
+
+def polynomial(x_k, p):
+    x = int(x_k[0])
+    k = int(x_k[1])
+    result = poly_sum(int(x), p)
+    if result == k:
+        return True
+    return False
+
+
+def main():
+    x_k = input().split(sep=" ")
+    p = input().split(sep=" ")
+    statement = polynomial(x_k, p)
+    if statement:
+        print(True)
+    else:
+        print(False)
+
+
+if __name__ == "__main__":
+    main()
