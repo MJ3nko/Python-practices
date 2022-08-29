69a59c97c29649dfbb23d364ba2f8feb2db53b0d
Neotar <neotar16@gmail.com>
2022-08-29 22:22:42 +0200

    Add set add method usage

---
+++ data_types/sets/set_add.py
@@ -0,0 +1,15 @@
+def distinct_countries(n):
+    countries = set()
+    distinct_countries = 0
+    for _ in range(n):
+        country = input()
+        if country not in countries:
+            distinct_countries += 1
+            countries.add(country)
+    return distinct_countries
+
+
+if __name__ == '__main__':
+    n = int(input())
+    result = distinct_countries(n)
+    print(result)
