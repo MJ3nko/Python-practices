425fbaf22797f03fad7d19f749ec8820d2c276be
Neotar <neotar16@gmail.com>
2023-03-24 23:39:45 +0100

    Add instruction for venv in Vscode

--- terminal_usage.sh
+++ terminal_usage.sh
@@ -5,11 +5,12 @@ python -m pip check                     # Check requirements
 pip freeze > requirements.txt           # Generate required packages file
 
 
-# DO NOT install into your global Python interpreter! 
+# DO NOT install into your global Python interpreter!.
 # ALWAYS try to use an environment when developing locally!
+# VSCODE - Create Environment
 # Unless you are developing in a container.
 
-python -m venv ./venv                   # Create virtual environment
+python -m venv ./venv                   # Create virtual environment in Linux
 apt-get install python3-pip             # Download and install pip
 pip3 install <package name>             # Download modules from PyPI
 pip3 show <package name>                # Detailed info about the package
