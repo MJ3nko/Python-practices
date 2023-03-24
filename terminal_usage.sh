8bf011d328164850a2f4d9454dce946718997974
Neotar <neotar16@gmail.com>
2023-03-24 13:13:25 +0100

    Change structure of the project

--- terminal_usage.sh
+++ terminal_usage.sh
@@ -1,7 +1,9 @@
 python -m pip list                      # List packages
 python -m pip install --upgrade pip     # Update pip in virtual environment
 python -m pip check                     # Check requirements
-/usr/bin/python3.7 -m pip               # Using pip with other Python interpreters
+/usr/bin/python3.11 -m pip               # Using pip with other Python interpreters
+pip freeze > requirements.txt           # Generate required packages file
+
 
 # DO NOT install into your global Python interpreter! 
 # ALWAYS try to use an environment when developing locally!
@@ -12,8 +14,7 @@ pip3 install <package name>             # Download modules from PyPI
 pip3 show <package name>                # Detailed info about the package
 python setup.py install                 # Install package directly
 
-# Generate required libraries list
 
-pip freeze > requirements.txt           # Generate required packages file
-
-# Formatter - use black instead of autopep8 that does minimal work
+# FORMATTER
+# Use black instead of autopep8 that does minimal work
+# 
