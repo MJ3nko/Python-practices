cbc2ac39378e7aff10ddc60873ca493478908d58
Neotar <neotar16@gmail.com>
2024-09-20 22:11:31 +0200

    Complex class rewrite

--- src/classes/examples/complex_class.py
+++ src/classes/examples/complex_class.py
@@ -1,49 +1,46 @@
 import math
+from typing import Union
 
 
 class Complex(object):
-    def __init__(self, real, imaginary):
-        self.real = real
-        self.imaginary = imaginary
+    def __init__(self, real: Union[float, int], imaginary: Union[float, int]) -> None:
+        self.real: Union[float, int] = real
+        self.imaginary: Union[float, int] = imaginary
 
-    def __add__(self, no):
+    def __add__(self, no: "Complex") -> "Complex":
         # Overloaded for + operation
-        a = self.real
-        b = self.imaginary
-        c = no.real
-        d = no.imaginary
+        a: Union[float, int] = self.real
+        b: Union[float, int] = self.imaginary
+        c: Union[float, int] = no.real
+        d: Union[float, int] = no.imaginary
         return Complex(a + c, b + d)
 
-    def __sub__(self, no):
+    def __sub__(self, no: "Complex") -> "Complex":
         # Overloaded for - operation
-        a = self.real
-        b = self.imaginary
-        c = no.real
-        d = no.imaginary
+        a: Union[float, int] = self.real
+        b: Union[float, int] = self.imaginary
+        c: Union[float, int] = no.real
+        d: Union[float, int] = no.imaginary
         return Complex(a - c, b - d)
 
-    def __mul__(self, no):
+    def __mul__(self, no: "Complex") -> "Complex":
         # Overloaded for * operation
-        a = self.real
-        b = self.imaginary
-        c = no.real
-        d = no.imaginary
-        real_mult = (a * c) - (b * d)
-        imag_mult = (a * d) + (b * c)
+        a: Union[float, int] = self.real
+        b: Union[float, int] = self.imaginary
+        c: Union[float, int] = no.real
+        d: Union[float, int] = no.imaginary
+        real_mult: Union[float, int] = (a * c) - (b * d)
+        imag_mult: Union[float, int] = (a * d) + (b * c)
         return Complex(real_mult, imag_mult)
 
-    def __truediv__(self, no):
+    def __truediv__(self, no: "Complex") -> "Complex":
         # Overloaded for / operation
-        a = self.real
-        b = self.imaginary
-        c = no.real
-        d = no.imaginary
-        real_numerator = a * c + b * d
-        imag_numerator = b * c - a * d
-        denom = c * c + d * d
-        real_div = real_numerator / denom
-        imag_div = imag_numerator / denom
-        return Complex(real_div, imag_div)
+        a: Union[float, int] = self.real
+        b: Union[float, int] = self.imaginary
+        c: Union[float, int] = no.real
+        d: Union[float, int] = no.imaginary
+        real_numerator: Union[float, int] = a * c + b * d
+        return real_numerator / (c**2 + d**2)
 
     def __str__(self):
         # Overloaded for str() operation
@@ -60,15 +57,20 @@ class Complex(object):
             result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
         return result
 
-    def mod(self):
-        a = self.real
-        b = self.imaginary
+    def mod(self) -> "Complex":
+        a: Union[float, int] = self.real
+        b: Union[float, int] = self.imaginary
         return Complex(math.sqrt(a**2 + b**2), 0)
 
 
 if __name__ == "__main__":
-    c = map(float, input().split())
-    d = map(float, input().split())
-    x = Complex(*c)
-    y = Complex(*d)
-    print(*map(str, [x + y, x - y, x * y, x / y, x.mod(), y.mod()]), sep="\n")
+    c = list(map(float, input().split()))
+    d = list(map(float, input().split()))
+    x = Complex(c[0], c[1])
+    y = Complex(d[0], d[1])
+    print(x + y)
+    print(x - y)
+    print(x * y)
+    print(x / y)
+    print(x.mod())
+    print(y.mod())
